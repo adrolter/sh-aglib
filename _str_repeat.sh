580e089fbc55edf368d4c2e6d91510c5a196835b
Adrian Günter <adrian@gntr.me>
2018-04-26 00:44:55 -0400

    Begin substring implementation

--- _str_repeat.sh
+++ _str_repeat.sh
@@ -1,1 +1,56 @@
-_str_repeat() { awk 'END{for(i=0;i<'${2-0}';i++)printf "%s","'"$1"'"}' </dev/null; }
+. "$AGLIB"/_char_repeat.sh
+
+# Fancy string repeat
+# Note: we use = to compare numbers for speed
+_str_repeat2() {
+  s="$1"
+  count="$2" # _test_parse_int ?
+  method="$3"
+  strlen=${#1}
+  # TODO Auto-select method based on string length and count
+  # Use awk when $count > 500 ? (only for long strings?) UNLESS printf is a builtin, then use printf-seq until ~800 or more?
+  # Use awk when $strlen > ?
+  # Most important aspect is to reduce iterations as much as possible to reduce builtin/external command calls
+  if [ -z "$method" ]; then
+     if [ $strlen = 1 ]; then method=char
+   else return 4
+     fi
+  fi
+  [ ${4-0} = 1 ] && >&2 printf 'Source: "%s"\nLength: %d\nCount: %d\nMethod: %s\n' "$s" $strlen $count "$method"
+  case "$method" in
+          char) _char_repeat "$1" $count; echo ;;
+    printf-seq) for i in $(seq $count); do printf %s "$s"; done ;;
+        static) case $count in # TODO Source a seperate file of compiled case statement from 2-100+
+                  2) printf %s "$s$s" ;;
+                  3) printf %s "$s$s$s" ;;
+                  4) printf %s "$s$s$s$s" ;;
+                  5) printf %s "$s$s$s$s$s" ;;
+                  6) printf %s "$s$s$s$s$s$s" ;;
+                  7) printf %s "$s$s$s$s$s$s$s" ;;
+                  8) printf %s "$s$s$s$s$s$s$s$s" ;;
+                  9) printf %s "$s$s$s$s$s$s$s$s$s" ;;
+                 10) printf %s "$s$s$s$s$s$s$s$s$s$s" ;;
+                 11) printf %s "$s$s$s$s$s$s$s$s$s$s$s" ;;
+                 12) printf %s "$s$s$s$s$s$s$s$s$s$s$s$s" ;;
+                 13) printf %s "$s$s$s$s$s$s$s$s$s$s$s$s$s" ;;
+                 14) printf %s "$s$s$s$s$s$s$s$s$s$s$s$s$s$s" ;;
+                 15) printf %s "$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s" ;;
+                 16) printf %s "$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s" ;;
+                 17) printf %s "$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s" ;;
+                 18) printf %s "$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s" ;;
+                 19) printf %s "$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s" ;;
+                 20) printf %s "$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s$s" ;;
+                esac
+              ;;
+          eval) t=; while :; do case ${#t} in $offset) break;; esac; t="$t\$s"; done; eval "printf %s \"$t\"" ;;
+       awk-var) awk -v s="$s" 'BEGIN{for(i=0;i<'${count}';i++)printf "%s",s}' ;;
+      awk-pipe) awk 'BEGIN{for(i=0;i<'${count}';i++)printf "%s",s}' ;;
+    concat-seq) t=; for i in $(seq $count); do t="$t$s"; done; printf %s "$t" ;;
+    perl-slurp) perl -0777pe '$_=$_ x '$count ;;
+             *) return 3;;
+  esac
+}
+
+# TODO String builder using a fifo
+# Background process (awk? dd?) reading from pipe, building a string efficiently in memory, then dumping it back to pipe
+# to be read back into main context variable somehow?
---
+++ _str_substr.sh
@@ -0,0 +1,43 @@
+_str_substr() {
+  s="$1"
+  offset="$2" # _test_parse_int ?
+  length="$3"
+  method="$4"
+  strlen=${#1}
+  # TODO Auto-select method based on string length and count
+  # TODO Remove or convert to case
+  [ ${5-0} = 1 ] && >&2 printf 'Source: "%s" (%d)\nOffset: %d\nLength: %d\nMethod: %s\n' "$s" $strlen $offset $length "$method"
+  # TODO: Shorten method strings to single character to speed up matching
+  case "$method" in
+  printf-from0) printf %.${length}s "$s" ;; # Only (and always) use if $offset = 0
+   eval-static) eval "case ${__match_any_${offset}+set} in " ;; # TODO finish
+        static) case $offset in
+                  # TODO Source a seperate file of compiled case statement from 2-100+
+                  #   ...or build it with eval and cache the result string
+                  1) printf %.${length}s "${s#?}" ;;
+                  2) printf %.${length}s "${s#??}" ;;
+                  3) printf %.${length}s "${s#???}" ;;
+                  4) printf %.${length}s "${s#????}" ;;
+                  5) printf %.${length}s "${s#?????}" ;;
+                  6) printf %.${length}s "${s#??????}" ;;
+                  7) printf %.${length}s "${s#???????}" ;;
+                  8) printf %.${length}s "${s#????????}" ;;
+                  9) printf %.${length}s "${s#?????????}" ;;
+                 10) printf %.${length}s "${s#??????????}" ;;
+                 11) printf %.${length}s "${s#???????????}" ;;
+                 12) printf %.${length}s "${s#????????????}" ;;
+                 13) printf %.${length}s "${s#?????????????}" ;;
+                 14) printf %.${length}s "${s#??????????????}" ;;
+                 15) printf %.${length}s "${s#???????????????}" ;;
+                 16) printf %.${length}s "${s#????????????????}" ;;
+                 17) printf %.${length}s "${s#?????????????????}" ;;
+                 18) printf %.${length}s "${s#??????????????????}" ;;
+                 19) printf %.${length}s "${s#???????????????????}" ;;
+                 20) printf %.${length}s "${s#????????????????????}" ;;
+                esac
+              ;;
+ printf-cmdsub) printf %.${length}s "${s#"$(printf %.${offset}s "$s")"}" ;;
+   printf-eval) t=; while :; do case ${#t} in $offset) break;; esac; t="$t?"; done; eval "printf %.${length}s \"\${s#$t}\"" ;;
+             *) return 3;;
+  esac
+}
