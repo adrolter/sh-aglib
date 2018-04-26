f14eef01c1e9ade37d5674cba90bce162080d8ef
Adrian Günter <adrianguenter@gmail.com>
2018-04-26 13:57:21 -0400

    Create _str_replace

---
+++ _str_replace.sh
@@ -0,0 +1,21 @@
+_str_replace() {
+  # TODO decide on func signature (order of pararms)
+  # TODO add "limit" paramter and break loop early if reached
+  t="e" # search
+  tlen=${#t}
+  r="ee" # replace
+  s="this is a test of the emergency broadcast system" # source
+
+  new_s=
+  while :; do
+    a="${s#*"$t"}"
+    #echo "s='$s'; a='$a'"
+    case "$s" in "$a") new_s="${new_s}${s}"; break;; esac
+    # TODO is it faster to printf in cmd sub for 2nd string ($s) or to remove $a from end of $s using ${s%"$a"} and simply concat it all?
+    # Might depend on length of strings being dealt with?
+    new_s="$(printf "%s%.$((${#s} - ${#a} - $tlen))s%s" "$new_s" "$s" "$r")"
+    s="$a"
+  done
+
+  printf %s "$new_s"
+}
